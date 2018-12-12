0b98327bfb3f73f891ea4b57a9ae1a64ba38319c
Emre <emreartis1996@gmail.com>
2018-12-12 23:55:23 +0300

    Update Communication.h

--- src/modules/communication/Communication.h
+++ src/modules/communication/Communication.h
@@ -1,20 +1,25 @@
-enum class ErrorStatus {
-    Success,
-    ErrorSetup,
-    ErrorConnect,
-    ErrorSend,
-    ErrorRecieve,
-    ErrorProcess,
-    ErrorClose
-    //...
-};
+namespace communication{
+    
+    enum class ErrorStatus {
+        Success,
+        SocketCreationFailed,
+        AddressInUse,
+        NotValidSocketDescriptor,
+        ConnectionRejected,
+        BufferSpaceNotAvailable,
+        OperationTimedOut,
+        SocketWasClosed
+        //
+    };
+    
+    class	Communication {
+        public:
+            virtual ErrorStatus setup() = 0;
+            virtual ErrorStatus connect() = 0;
+            virtual ErrorStatus send(char* message, int length) = 0;
+            virtual ErrorStatus recieve(char* message) = 0;
+            virtual ErrorStatus process(char* in_message, char* out_message) = 0;
+            virtual ErrorStatus close() = 0;
+    };
 
-class	Communication {
-    public:
-        virtual ErrorStatus setup() = 0;
-        virtual ErrorStatus connect() = 0;
-        virtual ErrorStatus send() = 0;
-        virtual ErrorStatus recieve() = 0;
-        virtual ErrorStatus process() = 0;
-        virtual ErrorStatus close() = 0;
-};
+}
