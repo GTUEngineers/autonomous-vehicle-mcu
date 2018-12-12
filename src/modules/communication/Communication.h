9180b0d87f905b3ebbc483ccc84231a4555d5de4
Emre <emreartis1996@gmail.com>
2018-12-13 00:56:31 +0300

    Update Communication.h

--- src/modules/communication/Communication.h
+++ src/modules/communication/Communication.h
@@ -17,7 +17,7 @@ namespace communication{
             virtual ErrorStatus setup() = 0;
             virtual ErrorStatus connect() = 0;
             virtual ErrorStatus send(char* message, int length) = 0;
-            virtual ErrorStatus recieve(char* message) = 0;
+            virtual ErrorStatus recieve(char* message,int length) = 0;
             virtual ErrorStatus process(char* in_message, char* out_message) = 0;
             virtual ErrorStatus close() = 0;
     };
