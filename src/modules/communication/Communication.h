885a051e068e722959397b92ac3d9b8806f5c064
Emre <emreartis1996@gmail.com>
2018-12-13 18:07:56 +0300

    Update Communication.h

--- src/modules/communication/Communication.h
+++ src/modules/communication/Communication.h
@@ -1,25 +1,35 @@
-namespace communication{
-    
-    enum class ErrorStatus {
-        Success,
-        SocketCreationFailed,
-        AddressInUse,
-        NotValidSocketDescriptor,
-        ConnectionRejected,
-        BufferSpaceNotAvailable,
-        OperationTimedOut,
-        SocketWasClosed
-        //
-    };
-    
-    class	Communication {
-        public:
-            virtual ErrorStatus setup() = 0;
-            virtual ErrorStatus connect() = 0;
-            virtual ErrorStatus send(char* message, int length) = 0;
-            virtual ErrorStatus recieve(char* message,int length) = 0;
-            virtual ErrorStatus process(char* in_message, char* out_message) = 0;
-            virtual ErrorStatus close() = 0;
-    };
+#ifndef AUTONOMOUS_VEHICLE_GTU_COMMUNICATION_H
+#define AUTONOMOUS_VEHICLE_GTU_COMMUNICATION_H
 
+namespace GTU {
+    namespace AUTONOMOUS_VEHICLE {
+
+        enum class ErrorStatus {
+            Success,
+            SocketCreationFailed,
+            AddressInUse,
+            NotValidSocketDescriptor,
+            ConnectionRejected,
+            BufferSpaceNotAvailable,
+            OperationTimedOut,
+            SocketWasClosed
+            //
+        };
+        
+        
+        //  Communication Interface
+        class	Communication {
+            public:
+                virtual ErrorStatus setup() = 0;
+                virtual ErrorStatus connect() = 0;
+                virtual ErrorStatus send(const char* message, const int length) = 0;
+                virtual ErrorStatus receive(char* message, const int length) = 0;
+                virtual ErrorStatus process(const char* in_message, char* out_message) = 0;
+                virtual ErrorStatus close() = 0;
+        };
+        
+    }    
 }
+
+
+#endif //AUTONOMOUS_VEHICLE_GTU_COMMUNICATION_H
