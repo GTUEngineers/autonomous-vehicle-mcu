578953ed3a0f5ec87a2842be71a81400ebd9aca5
Ahmet Alperen BULUT <bltalperen@gmail.com>
2019-07-19 14:48:09 +0300

    PWM was reconfigured

--- STM32_Codes/autonomousVehicle_GTU/Src/main.c
+++ STM32_Codes/autonomousVehicle_GTU/Src/main.c
@@ -338,7 +338,7 @@ static void MX_TIM2_Init (void)
 
     /* USER CODE END TIM2_Init 1 */
     htim2.Instance = TIM2;
-    htim2.Init.Prescaler = 32;
+    htim2.Init.Prescaler = 332;
     htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
     htim2.Init.Period = 100;
     htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
