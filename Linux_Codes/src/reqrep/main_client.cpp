0e7d157881867f20d05f7cdbea58a86f7a906102
sumeyye <taskansumeyye@gmail.com>
2019-08-10 17:23:59 +0300

    create protobuf for Station and Car

--- Linux_Codes/src/reqrep/main_client.cpp
+++ Linux_Codes/src/reqrep/main_client.cpp
@@ -1,4 +1,5 @@
 #include "client.h"
+#include "station_car.pb.h"
 #include <iostream>
 #include <memory>
 #include <unistd.h>
@@ -13,7 +14,17 @@ int main()
     //a counter to counts requests and responses
     int counter = 0;
     while (true) {
-        zmq::message_t request("alperen", 7);
+
+        std::string adinisenkoy;
+        seq_req_rep reqrep;
+        std::unique_ptr<Commands> commands(new Commands);
+        std::unique_ptr<Command_req> cmd_req(new Command_req);
+        cmd_req->set_cmd(cmd_enum::STOP);
+        commands->set_allocated_req(cmd_req.release());
+        reqrep.set_allocated_cmd_msg(commands.release());
+        reqrep.SerializeToString(&adinisenkoy);
+
+        zmq::message_t request(adinisenkoy.c_str(), adinisenkoy.size());
         std::cout << "Sending Hello: " << counter << std::endl;
         zmq::message_t reply;
         //if connection is not broken
