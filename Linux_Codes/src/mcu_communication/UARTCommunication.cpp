a09ad656e7af6c47392a54fdeeb59a7ddeafef4c
sevvalmehder <smehder@gtu.edu.tr>
2019-08-17 23:45:05 +0300

    Basic MCU Communication Class

---
+++ Linux_Codes/src/mcu_communication/UARTCommunication.cpp
@@ -0,0 +1,34 @@
+/**
+ * \file        UARTCommunication.cpp
+ * \brief       A brief description one line.
+ *
+ * \author      Sevval MEHDER
+ * \date        Aug 17, 2019
+ */
+
+/*------------------------------< Includes >----------------------------------*/
+#include "UARTCommunication.h"
+
+/*------------------------------< Defines >-----------------------------------*/
+
+/*------------------------------< Typedefs >----------------------------------*/
+
+/*------------------------------< Namespaces >--------------------------------*/
+
+
+UARTCommunication::UARTCommunication(){
+	// Init function
+	this->isConnected = false;
+}
+
+void UARTCommunication::transmit(std::string message){
+	// Transmit operation
+}
+
+std::string UARTCommunication::receive(){
+	// Receive operation
+}
+
+
+
+
---
+++ Linux_Codes/src/mcu_communication/UARTCommunication.h
@@ -0,0 +1,36 @@
+/**
+ * \file        UARTCommunication.h
+ * \brief       A brief description one line.
+ *
+ * \author      Sevval MEHDER
+ * \date        Aug 17, 2019
+ */
+
+#ifndef UART_COMMUNICATION_H_
+#define UART_COMMUNICATION_H_
+
+
+/*------------------------------< Includes >----------------------------------*/
+#include <string>
+
+/*------------------------------< Defines >-----------------------------------*/
+
+/*------------------------------< Typedefs >----------------------------------*/
+
+/*------------------------------< Class  >------------------------------------*/
+
+class UARTCommunication {
+public:
+	UARTCommunication(); /* Constructor */
+	std::string receive();
+	void transmit(std::string message);
+private:
+	bool isConnected;
+	std::string lastMessage;
+};
+
+
+
+
+
+#endif /* UART_COMMUNICATION_H_ */
