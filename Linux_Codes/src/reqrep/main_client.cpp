d0d82b92d01ffa8f5bc1f5080b5640992efeab95
Ongun Alp Baba <ongunalpbb@gmail.com>
2019-08-21 01:02:14 +0300

    HCSR4, GPS, StateWorking create functions coded on main_client.cpp

--- Linux_Codes/src/reqrep/main_client.cpp
+++ Linux_Codes/src/reqrep/main_client.cpp
@@ -37,12 +37,58 @@ std::string create_cmd_req(cmd_enum cmd)
     return ret_str;
 }
 
-bool parse_cmd_rep(std::string& rep, ReturnCode& retCode)
+std::string create_statework(stateWorking_enum cmd)
+{
+    std::string ret_str;
+    pub_sub pubsub;
+    std::unique_ptr<StateWorking> stateworks(new StateWorking);
+
+    stateworks->set_cmd(cmd);
+
+    pubsub.set_allocated_statework(stateworks.release());
+
+    pubsub.SerializeToString(&ret_str);
+    return ret_str;
+}
+
+std::string create_location(float latitude, float longitude)
+{
+    std::string ret_str;
+    pub_sub pubsub;
+    std::unique_ptr<GPS> gps(new GPS);
+
+    gps->set_latitude(latitude);
+    gps->set_longitude(longitude);
+
+    pubsub.set_allocated_location(gps.release());
+
+    pubsub.SerializeToString(&ret_str);
+    return ret_str;
+}
+
+std::string create_hcsr4_dis(double distance)
+{
+    std::string ret_str;
+    pub_sub pubsub;
+    std::unique_ptr<HCSR4> hcsr4(new HCSR4);
+
+    hcsr4->set_distance(distance);
+
+    pubsub.set_allocated_hcsr4_dis(hcsr4.release());
+
+    pubsub.SerializeToString(&ret_str);
+    return ret_str;
+}
+
+bool parse_cmd_rep(std::string &rep, ReturnCode &retCode)
 {
     seq_req_rep reqrep;
-    if (reqrep.ParseFromArray(rep.data(), rep.size())) {
-        if (reqrep.has_cmd_msg()) {
-            if (reqrep.cmd_msg().has_rep()) {
+    if (reqrep.ParseFromArray(rep.data(), rep.size()))
+    {
+        if (reqrep.has_cmd_msg())
+        {
+            if (reqrep.cmd_msg().has_rep())
+            {
                 retCode = reqrep.cmd_msg().rep().retval();
                 return true;
             }
@@ -54,7 +100,7 @@ bool parse_cmd_rep(std::string& rep, ReturnCode& retCode)
 //Driver file for Client
 int main()
 {
-    std::shared_ptr<spdlog::logger> m_logger{ spdlog::stdout_color_mt("REQREP_Client") };
+    std::shared_ptr<spdlog::logger> m_logger{spdlog::stdout_color_mt("REQREP_Client")};
 
     m_logger->set_level(spdlog::level::debug);
     std::unique_ptr<seqreqrep::Client> client(new seqreqrep::Client);
@@ -67,21 +113,24 @@ int main()
     client->connect(addr);
     //a counter to counts requests and responses
 
-    while (true) {
+    while (true)
+    {
         std::string req = create_cmd_req(cmd_enum::START);
         zmq::message_t request(req.c_str(), req.size());
 
         zmq::message_t reply;
         //if connection is not broken
-        if (client->reqrep(request, reply, 3)) {
+        if (client->reqrep(request, reply, 3))
+        {
             ReturnCode retCode;
-            std::string rep((char*)reply.data(), reply.size());
+            std::string rep((char *)reply.data(), reply.size());
             parse_cmd_rep(rep, retCode);
 
             m_logger->debug("Server Rep: {}", retCode);
         }
         //if it is broken
-        else {
+        else
+        {
             m_logger->critical("Connection was Broken");
             //resets client
             client.reset(new seqreqrep::Client);
