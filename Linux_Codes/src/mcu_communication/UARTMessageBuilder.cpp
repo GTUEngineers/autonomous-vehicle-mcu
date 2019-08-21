c4a0f38da63ef90398443d99b39499429346ec08
Bulut, Ahmet Alperen <bltalperen@gmail.com>
2019-08-21 22:38:25 +0300

    UART message done

--- Linux_Codes/src/mcu_communication/UARTMessageBuilder.cpp
+++ Linux_Codes/src/mcu_communication/UARTMessageBuilder.cpp
@@ -8,28 +8,129 @@
 
 /*------------------------------< Includes >----------------------------------*/
 #include "UARTMessageBuilder.h"
+#include "process.pb.h"
 #include <iostream>
+#include <memory>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/syslog_sink.h>
-
+#include <spdlog/spdlog.h>
 /*------------------------------< Defines >-----------------------------------*/
 
-/*------------------------------< Typedefs >----------------------------------*/
-
+/*------------------------------< Variables >----------------------------------*/
+std::shared_ptr<spdlog::logger> m_logger;
 /*------------------------------< Namespaces >--------------------------------*/
+void reverse(uart_req& req);
 
-UARTMessageBuilder::UARTMessageBuilder()
+void init_uartmessagebuilder_logger()
 {
-    m_logger = spdlog::stdout_color_mt("UARTMessageBuilder");
-    m_logger->set_level(spdlog::level::debug);
-    m_logger->info("Serial Port:{}", serial_port);
-
+    m_logger = spdlog::stdout_color_mt("UARTCommunication");
+}
+void reverse(uart_req& req)
+{
+    uint8_t l = req.req.msg[1];
+    req.req.msg[1] = req.req.msg[0];
+    req.req.msg[0] = l;
 }
 
-UARTMessageBuilder::~UARTMessageBuilder()
+uart_req create_steer_msg(const uart::steering_enum dir, const uint16_t& val)
 {
-    try {
+    uart_req req;
+    req.req_uint16.msg = val;
+    reverse(req);
+    uint8_t header = (uint8_t)dir;
+    req.req.msg[0] = ((req.req.msg[0] & 0b00000011) | (header << 2) | 0b00010000);
+    return req;
+}
 
-    } catch (...) {
-    }
+uart_req create_throttle_msg(const uint8_t& val)
+{
+    uart_req req;
+    memcpy(&req.req.msg[1], &val, sizeof(uint8_t));
+    req.req.msg[0] = 0b00100000;
+    return req;
+}
+
+uart_req create_brake_msg(const uart::brake_enum val)
+{
+    uart_req req;
+    req.req.msg[0] = 3 << 4;
+    req.req.msg[1] = val;
+    return req;
+}
+
+uart_req create_startstop_msg(const uart::startstop_enum val)
+{
+    uart_req req;
+    req.req.msg[0] = 0;
+    req.req.msg[1] = val;
+    return req;
+}
+
+uart_req create_state_msg()
+{
+    uart_req req;
+    req.req.msg[0] = 64;
+    return req;
+}
+
+uart_req create_hcsr4_msg()
+{
+    uart_req req;
+    req.req.msg[0] = 80;
+    return req;
+}
+uart_req create_gps_msg()
+{
+    uart_req req;
+    req.req.msg[0] = 96;
+    return req;
+}
+
+uart::stateWorking_enum parse_state_msg(const uart_rep& msg)
+{
+    return (uart::stateWorking_enum)msg.rep.msg[1];
+}
+
+std::string parse_hcsr4_msg(const uart_rep& msg)
+{
+}
+
+uart_rep parse_gps_msg(const uart_rep& msg)
+{
+    return msg;
+}
+
+void parse_general_rep_msg(const uart_rep& msg, uint8_t& val)
+{
+    val = (bool)msg.rep.msg[1];
+}
+
+void parse_startstop_rep_msg(const uart_rep& msg, uint8_t& val)
+{
+    val = (bool)msg.rep.msg[1];
+}
+
+void parse_steer_msg(const uart_req& req, uint8_t& dir, uint16_t& val)
+{
+#define DIR_MASK (0b000000100)
+#define ANGLE_MASK (0b00000011111111)
+    uint16_t temp = req.req.msg[0];
+    dir = (temp & DIR_MASK) >> 2;
+    uart_req reqm = req;
+    reverse(reqm);
+    val = reqm.req_uint16.msg & ANGLE_MASK;
+#undef DIR_MASK
+#undef ANGLE_MASK
+}
+
+void parse_throttle_msg(const uart_req& req, uint8_t& val)
+{
+    val = req.req.msg[1];
+}
+
+void parse_brake_msg(const uart_req& req, uint8_t& val)
+{
+#define BRAKE_MASK (0b00000001)
+    val = (req.req.msg[1] & BRAKE_MASK);
+#undef BRAKE_MASK
 }
--- Linux_Codes/src/mcu_communication/UARTMessageBuilder.h
+++ Linux_Codes/src/mcu_communication/UARTMessageBuilder.h
@@ -10,23 +10,59 @@
 #define UART_MESSAGEBUILDER_H_
 
 /*------------------------------< Includes >----------------------------------*/
-#include <memory>
-#include <spdlog/spdlog.h>
+#include "process.pb.h"
 #include <string>
 /*------------------------------< Defines >-----------------------------------*/
+struct UART_req {
+    uint8_t msg[2];
+} __attribute__((packed, aligned(1)));
 
-/*------------------------------< Typedefs >----------------------------------*/
+struct UART_req_uint16 {
+    uint16_t msg;
+} __attribute__((packed, aligned(1)));
 
-/*------------------------------< Class  >------------------------------------*/
-
-class UARTMessageBuilder
-{
-public:
-    UARTMessageBuilder(); /* Constructor */
-    ~UARTMessageBuilder();
-  
-private:
-    std::shared_ptr<spdlog::logger> m_logger;
+union UART_req_un {
+    struct UART_req req;
+    struct UART_req_uint16 req_uint16;
 };
+struct UART_rep {
+    uint8_t msg[9];
+} __attribute__((packed, aligned(1)));
 
+struct UART_rep_uint16 {
+    uint8_t header;
+    uint16_t msg[4];
+} __attribute__((packed, aligned(1)));
+
+struct GPS {
+    uint8_t header;
+    float latitude;
+    float longitude;
+} __attribute__((packed, aligned(1)));
+
+union UART_rep_un {
+    struct UART_rep rep;
+    struct UART_rep_uint16 rep_uint16;
+    struct GPS gps;
+};
+/*------------------------------< Typedefs >----------------------------------*/
+typedef union UART_req_un uart_req;
+typedef union UART_rep_un uart_rep;
+/*------------------------------< Class  >------------------------------------*/
+void init_uartmessagebuilder_logger();
+uart_req create_steer_msg(const uart::steering_enum dir, const uint16_t& val);
+uart_req create_throttle_msg(const uint8_t& val);
+uart_req create_brake_msg(const uart::brake_enum val);
+uart_req create_startstop_msg(const uart::startstop_enum val);
+uart_req create_state_msg();
+uart_req create_hcsr4_msg();
+uart_req create_gps_msg();
+bool parse_general_rep_msg(const uart_rep& msg);
+uart::stateWorking_enum parse_state_msg(const uart_rep& msg);
+std::string parse_hcsr4_msg(const uart_rep& msg);
+uart_rep parse_gps_msg(const uart_rep& msg);
+void parse_steer_msg(const uart_req& req, uint8_t& dir, uint16_t& val);
+void parse_throttle_msg(const uart_req& req, uint8_t& val);
+void parse_brake_msg(const uart_req& req, uint8_t& val);
+void parse_startstop_rep_msg(const uart_rep& msg, uint8_t& val);
 #endif /* UART_MESSAGEBUILDER_H_ */
