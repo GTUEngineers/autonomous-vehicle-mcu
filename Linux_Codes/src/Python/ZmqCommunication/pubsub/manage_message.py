17741efb8399c5feb8d9653e6b1fb5360011f515
Tolga <tolgaareis@gmail.com>
2019-08-21 08:52:46 +0300

    creat_message rearranged

--- Linux_Codes/src/Python/ZmqCommunication/pubsub/manage_message.py
+++ Linux_Codes/src/Python/ZmqCommunication/pubsub/manage_message.py
@@ -6,31 +6,7 @@ import process_pb2
 import common_pb2
 import logging
 
-#message is
-def create_message(pubsub_message):
-    retMsg = ""
-    if pubsub_message.msg_type is process_pb2.pub_sub_message.START_STOP_MSG:
-        startstop_message = process_pb2.StartStop_req()
-        startstop_message.cmd = pubsub_message.startstop.cmd
-        retMsg = startstop_message.SerializeToString()
-    
-    elif pubsub_message.msg_type is process_pb2.pub_sub_message.THROTTLE_MSG:
-        throttle_message = process_pb2.Throttle_req()
-        throttle_message.throttleValue = pubsub_message.throttle.throttleValue
-        retMsg = throttle_message.SerializeToString()
-    
-    elif pubsub_message.msg_type is process_pb2.pub_sub_message.STEERING_MSG:
-        steering_message = process_pb2.Steering_req()
-        steering_message.cmd = steering_message.steering.cmd
-        steering_message.angle = pubsub_message.steering.angle
-        retMsg = steering_message.SerializeToString()
-    
-    elif pubsub_message.msg_type is process_pb2.pub_sub_message.BRAKE_MSG:
-        brake_message = process_pb2.Brake_req()
-        brake_message.brakeValue = pubsub_message.brake.brakeValue
-        retMsg = brake_message.SerializeToString()
-    
-    return retMsg
+#MAY process_pb2 NAME SHORTEN
 
 def create_startstop_msg(cmd):
     pubsub_message = process_pb2.pub_sub()
@@ -161,6 +137,50 @@ def parse_all_type_messages(msg):#alternatif olarak bütün mesaj tiplerini pars
 
 #YAZILAN KISIM BİTTİ
 
+#Author: Tolga
+#This method must take message_type as process_pb2.pubsub_message
+#val_1 is also must because all message types take at least one parameter
+#va_2 is optional according to message type
+def create_message(message_type, val_1, val_2 = None):
+     
+      pubsub_message = process_pb2.pub_sub()  
+
+      if message_type == process_pb2.pubsub_message.START_STOP_MSG:
+          pubsub_message.msg_type = process_pb2.pub_sub_message.START_STOP_MSG
+          pubsub_message.startstop.cmd = val_1 #val1 is cmd in here
+
+      elif message_type == process_pb2.pubsub_message.THROTTLE_MSG:
+          pubsub_message.msg_type = process_pb2.pub_sub_message.THROTTLE_MSG
+          pubsub_message.throttle.throttleValue = val_1 #val1 is throttleVal in here
+
+      elif message_type == process_pb2.pubsub_message.STEERING_MSG:
+          pubsub_message.msg_type = process_pb2.pub_sub_message.STEERING_MSG
+          pubsub_message.steering.cmd = val_1 #val1 is cmd in here
+          pubsub_message.steering.angle = val_2 #val2 is angle in here
+
+      elif message_type == process_pb2.pubsub_message.BRAKE_MSG:
+          pubsub_message.msg_type = process_pb2.pub_sub_message.BRAKE_MSG
+          pubsub_message.brake.brakeValue = val_1 #val1 is brakeValue in here
+
+      elif message_type == process_pb2.pubsub_message.STATE_WORKING_MSG:
+          pubsub_message.msg_type = process_pb2.pub_sub_message.STATE_WORKING_MSG
+          pubsub_message.statework.cmd = val_1 #val1 is cmd in here
+
+      elif message_type == process_pb2.pubsub_message.HSCR4_MSG:
+          pubsub_message.msg_type = process_pb2.pub_sub_message.HCSR4_MSG
+          pubsub_message.hcsr4_dis.distance = val_1 #val1 is distance in here
+
+      elif message_type == process_pb2.pubsub_message.GPS_MSG:    
+          pubsub_message.msg_type = process_pb2.pub_sub_message.GPS_MSG
+          pubsub_message.location.latitude = val_1 #val1 is latitude in here
+          pubsub_message.location.longitude = val_2 #val1 is longitude in here
+
+      else:
+          logging.warning("Invalid message type!")
+
+      return pubsub_message.SerializeToString()  
+#End of Tolga
+
 def testFoo():
 
     #test = create_steering_msg(process_pb2.steering_enum.LEFT,120.0)
@@ -184,27 +204,26 @@ def testFoo():
     #test = create_startstop_msg(process_pb2.startstop_enum.START)
     #test_pubsub = parse_startstop_msg(test)
 
-    test = create_steering_msg(process_pb2.steering_enum.RIGHT,360.0)
+    test = create_message(process_pb2.pub_sub_message.STEERING_MSG, process_pb2.steering_enum.RIGHT, 360.0)
     test_pubsub = parse_all_type_messages(test)
+
     if test_pubsub is not None:
         print("YEES "+str(test_pubsub))
-    test = create_startstop_msg(process_pb2.startstop_enum.STOP)
+
+    test = create_message(process_pb2.pub_sub_message.START_STOP.MSG, process_pb2.startstop_enum.STOP)
     test_pubsub = parse_startstop_msg(test)
+
     if test_pubsub is not None:
         print("YEES "+str(test_pubsub))
 
 testFoo()
 
 
-"""
-def parse_message(message):
-    pub_sub = process_pb2.pub_sub()
-    if pub_sub.msg_type == process_pb2.pub_sub_message.STATE_WORKING_MSG:
-        print("")
-    elif pub_sub.msg_type == pub_sub_message.HCSR4_MSG:
-        print("")
-    elif pub_sub_message == pub_sub_message.GPS_MSG:
-        print("")
-    else:
-        logging.critical("INVALID INCOMING MESSAGE!")
-"""
+
+
+
+
+
+
+
+
