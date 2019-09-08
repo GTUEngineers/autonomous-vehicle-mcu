e1e7201f22119f8067da46e3d1b343c0be8a15e8
Ongun Alp Baba <ongunalpbb@gmail.com>
2019-09-08 11:56:05 +0300

    LidarComm_client.py update

--- Linux_Codes/src/Python/CommonLib/CommonLib.py
+++ Linux_Codes/src/Python/CommonLib/CommonLib.py
@@ -1,23 +1,24 @@
-import lidar_comm_pb2
+# some_file.py
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, '../')
+from ProtoOut.lidar_comm_pb2 import *
 
 def create_clusters(cluster_list):
-    data = lidar_comm_pb2.LidarComm.pub_sub()
+    data = pub_sub()
     for sublist in cluster_list:
-        data.clusters.add()
-        data.clusters = lidar_comm_pb2.LidarComm.Cluster()
-        data.clusters.center = lidar_comm_pb2.LidarComm.Position()
-        data.clusters.center.x = sublist[0]
-        data.clusters.center.y = sublist[1]
-        data.clusters.center.z = sublist[2]
-        data.clusters.size = lidar_comm_pb2.LidarComm.Size()
-        data.clusters.size.width = sublist[3]
-        data.clusters.size.height = sublist[4]
-    return data.SerializeToStringD()
+        data_new = data.clusters.add()
+        data_new.center.x = sublist[0]
+        data_new.center.y = sublist[1]
+        data_new.center.z = sublist[2]
+        data_new.size.width = sublist[3]
+        data_new.size.height = sublist[4]
+    return data.SerializeToString()
 
 def parse_clusters(msg):
     cluster_list = []
     i = 0
-    data = lidar_comm_pb2.LidarComm.pub_sub()
+    data = pub_sub()
     data.ParseFromString(msg)
     for cluster in data.clusters:
         cluster_list.append([])
@@ -30,34 +31,28 @@ def parse_clusters(msg):
     return cluster_list
 
 def create_lidar_req(x1, y1, x2, y2):
-    request = lidar_comm_pb2.LidarComm.req_rep()
-    request.lidar_req = lidar_comm_pb2.LidarComm.RequestData()
-    request.lidar_req.upper_left = lidar_comm_pb2.LidarComm.Pixel()
+    request = req_rep()
     request.lidar_req.upper_left.x = x1
     request.lidar_req.upper_left.y = y1
-    request.lidar_req.downer_right = lidar_comm_pb2.LidarComm.Pixel()
     request.lidar_req.downer_right.x = x2
     request.lidar_req.downer_right.y = y2
     return request.SerializeToString()
 
 def parse_lidar_req(msg):
-    request = lidar_comm_pb2.LidarComm.req_rep()
+    request = req_rep()
     request.ParseFromString(msg)
     return request.lidar_req.upper_left.x, request.lidar_req.upper_left.y, request.lidar_req.downer_right.x, request.lidar_req.downer_right.y
 
 def create_lidar_rep(x, y, z, width, height):
-    reply = lidar_comm_pb2.LidarComm.req_rep()
-    reply.lidar_rep = lidar_comm_pb2.LidarComm.Cluster()
-    reply.lidar_rep.center = lidar_comm_pb2.LidarComm.Position()
+    reply = req_rep()
     reply.lidar_rep.center.x = x
     reply.lidar_rep.center.y = y
     reply.lidar_rep.center.z = z
-    reply.lidar_rep.size = lidar_comm_pb2.LidarComm.Size()
     reply.lidar_rep.size.width = width
     reply.lidar_rep.size.height = height
     return reply.SerializeToString()
 
 def parse_lidar_rep(msg):
-    reply = lidar_comm_pb2.LidarComm.req_rep()
+    reply = req_rep()
     reply.ParseFromString(msg)
     return reply.lidar_rep.center.x, reply.lidar_rep.center.y, reply.lidar_rep.center.z, reply.lidar_rep.size.width, reply.rep.size.height 
